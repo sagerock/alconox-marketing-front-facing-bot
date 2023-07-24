955c9a58a263c269fafd282c517a171772379889
sagerock <sage@sagerock.com>
2023-07-24 11:23:26 -0400

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,6 +4,13 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
+    <div className="mx-auto flex flex-col space-y-4">
+      <header className="container sticky top-0 z-40 bg-white">
+        <div className="h-16 border-b border-b-slate-200 py-4">
+          {/* The "Home" link and its parent elements were here, but they've been removed */}
+        </div>
+      </header>
+      <div>
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           {children}
         </main>
